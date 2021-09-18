41e5f85039a48ae31a2d23e104759e7f61712e2c
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-18 11:36:12 +0530

    added covid service api interface

---
+++ app/src/main/java/com/example/covid19tracker/network/remote/api/CovidTrackerService.kt
@@ -0,0 +1,26 @@
+package com.example.covid19tracker.network.remote.api
+
+import com.example.covid19tracker.network.remote.response.CovidData
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+/**
+ * Created by Dhruv Limbachiya on 14-09-2021.
+ */
+
+interface CovidTrackerService {
+
+    @GET("country/{country_name}")
+    suspend fun getCovidDataByCountry(
+        @Path("country_name") countryName : String
+    ) : List<CovidData>
+
+    @GET("country/{country_name}")
+    suspend fun getAllCovidHistoricalData(
+        @Path("country_name") countryName : String,
+        @Query("from") fromDate : String,
+        @Query("to") toDate : String,
+    ) : List<CovidData>
+
+}
