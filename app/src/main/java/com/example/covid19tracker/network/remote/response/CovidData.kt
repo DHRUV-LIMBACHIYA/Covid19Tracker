1aef9ae70fdd3dea4dceb75aaae7ddf5b8dbe2a6
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-18 11:18:11 +0530

    create data for covid service response.

---
+++ app/src/main/java/com/example/covid19tracker/network/remote/response/CovidData.kt
@@ -0,0 +1,17 @@
+package com.example.covid19tracker.network.remote.response
+
+data class CovidData(
+    val Active: Int,
+    val City: String,
+    val CityCode: String,
+    val Confirmed: Int,
+    val Country: String,
+    val CountryCode: String,
+    val Date: String,
+    val Deaths: Int,
+    val ID: String,
+    val Lat: String,
+    val Lon: String,
+    val Province: String,
+    val Recovered: Int
+)
