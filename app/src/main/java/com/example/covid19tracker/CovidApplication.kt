4fe609ba1e1106061423df8605c0cfb9d1e1a5d8
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-09-18 11:16:25 +0530

    Foundation setup for dagger-hilt

---
+++ app/src/main/java/com/example/covid19tracker/CovidApplication.kt
@@ -0,0 +1,11 @@
+package com.example.covid19tracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Dhruv Limbachiya on 14-09-2021.
+ */
+
+@HiltAndroidApp
+class CovidApplication : Application()
---
+++ app/src/main/java/com/example/covid19tracker/di/module/AppModule.kt
@@ -0,0 +1,14 @@
+package com.example.covid19tracker.di.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Created by Dhruv Limbachiya on 14-09-2021.
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
